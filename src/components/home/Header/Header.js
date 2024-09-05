b73798de2c7db684d24c63b2d325e6820b614cf4
shreya04kri <shreya24singhs@gmail.com>
2024-09-05 20:21:36 +0530

    Initial commit

--- src/components/home/Header/Header.js
+++ src/components/home/Header/Header.js
@@ -32,7 +32,7 @@ const Header = () => {
         <Flex className="flex items-center justify-between h-full">
           <Link to="/">
             <div>
-              <Image className="w-32 object-cover" imgSrc={logo} />
+              <Image className="invert h-auto w-32 object-cover" imgSrc={logoLight} />
             </div>
           </Link>
           <div>
